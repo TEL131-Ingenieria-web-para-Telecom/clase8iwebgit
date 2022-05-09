57ffcb4aaab4c0bc3b809b4dec35fd6f05dc5b3b
Stuardo Lucho Hp Omen <stuardo.lucho@pucp.edu.pe>
2022-05-09 17:14:49 -0500

    proycto base

---
+++ src/com/company/Gato.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Gato extends Animal{
+    private int alturaSalto;
+    private int numeroVidas = 8;
+
+    public int getAlturaSalto() {
+        return alturaSalto;
+    }
+
+    public void setAlturaSalto(int alturaSalto) {
+        this.alturaSalto = alturaSalto;
+    }
+
+    public int getNumeroVidas() {
+        return numeroVidas;
+    }
+
+    public void setNumeroVidas(int numeroVidas) {
+        this.numeroVidas = numeroVidas;
+    }
+}
